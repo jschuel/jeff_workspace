d6ca0d0836aa81f45a9146d39fdc965577c0411a
Jeffrey Schueler <jeffrey.schueler@desy.de>
2020-02-21 10:36:10 +0900

    wrote ntuple generator with accurate timing and much fasting processing

--- data_processing/generate_ntuples.py
+++ data_processing/generate_ntuples.py
@@ -9,24 +9,27 @@ import pandas as pd
 from ntuple_maker_module import create_ntuple
 import sys
 
+
 month = sys.argv[1]
 day = sys.argv[2]
+speed = sys.argv[3]
 
-sides = ["FWD", "BWD"]
+#sides = ["FWD", "BWD"]
+sides = ["BWD"]
 for side in sides:
     if side == "FWD":
         TPCs = ["nene","humu","iiwi"]
         #run_num = [i for i in range(24740,24749)] + [i for i in range(24827,24890)] #for dec 7th study
         run_num = [i for i in range(34745,34820)]
     if side == "BWD":
-        TPCs = ["elepaio", "palila", "tako"]
+        #TPCs = ["elepaio", "palila", "tako"]
+        TPCs = ["elepaio"]
+        run_num = [346]
         #run_num = [343,344,345,346] #for dec 7th study
-        run_num = [348,349,350] #for dec 8th study
+        #run_num = [348,349,350] #for dec 8th study
     for tpc in TPCs:
         for run in run_num:
             f_input = '/Users/vahsengrouplaptop/data/phase3/phase3_background_h5/%s_%s/%s_%s_stop_mode_ext_trigger_scan_interpreted.h5'%(month, day, run, tpc) 
-            f_output = '/Users/vahsengrouplaptop/data/phase3/phase3_background_root/%s_%s/%s_%s.root'%(month, day, run, tpc)
-            create_ntuple(f_input, f_output)
-
-
-
+            #f_output = '/Users/vahsengrouplaptop/data/phase3/phase3_background_root/%s_%s/%s_%s.root'%(month, day, run, tpc)
+            f_output = 'output/%s/%s_%s.root'%(speed,tpc,run)
+            create_ntuple(f_input, f_output, speed)
--- data_processing/ntuple_maker_module.py
+++ data_processing/ntuple_maker_module.py
@@ -3,7 +3,8 @@ After the root file is made, it can be passed into TPC_tools for final processin
 Branches generated with this function are:
 
 event_number
-timestamp
+timestamp_start
+timestamp_stop
 npoints
 column
 row
@@ -23,8 +24,15 @@ import matplotlib.pyplot as plt
 import sklearn.cluster as cluster
 import ROOT
 
-def create_ntuple(f_input, f_output):
-    input_df = extract_parameters_from_h5_file(f_input)
+def create_ntuple(f_input, f_output, speed):
+    if speed == "fast":
+        input_df = quick_h5_extract(f_input)
+        maximum = 10000
+    elif speed == "slow":
+        input_df = extract_parameters_from_h5_file(f_input)
+        maximum = 100000
+    else:
+        print("Please enter fast or slow")
     df = separate_events(input_df) #dataframe with multi-track events separated into single events. Call df for short
     #order: event_number, column, row, tot, BCID, timestamp, npoints, sum_tot, num_clusters
     output = ROOT.TFile(f_output, 'recreate')
@@ -34,10 +42,10 @@ def create_ntuple(f_input, f_output):
     root_npoints = array.array('i',[0])
     root_sum_tot = array.array('i',[0])
     root_num_clusters = array.array('i',[0])
-    root_col = array.array('i' , [0 for i in range(0,100000)])
-    root_row = array.array('i' , [0 for i in range(0,100000)])
-    root_tot = array.array('i' , [0 for i in range(0,100000)])
-    root_BCID = array.array('i' , [0 for i in range(0,100000)])
+    root_col = array.array('i' , [0 for i in range(0,maximum)])
+    root_row = array.array('i' , [0 for i in range(0,maximum)])
+    root_tot = array.array('i' , [0 for i in range(0,maximum)])
+    root_BCID = array.array('i' , [0 for i in range(0,maximum)])
     tree.Branch("event_number", root_evt, "event_number/I")
     tree.Branch("timestamp", root_ts , "timestamp/D")
     tree.Branch("npoints", root_npoints , "npoints/I")
@@ -69,7 +77,7 @@ def separate_events(dataframe): #takes read in dataframe and separates multitrac
     dataframe['num_clusters'] = pd.Series(1, index=dataframe.index)
     evts = [i for i in range(0,len(dataframe))] #numbers events by dataframe index
     dataframe['event_number']=evts #renumbers events so they match dataframe indices
-    events = dataframe.loc[dataframe['sum_tot']>800].index #selection for events we separate tracks from
+    events = dataframe.loc[dataframe['sum_tot']>800].index.to_numpy() #selection for events we separate tracks from
     col = dataframe['column']
     row = dataframe['row']
 
@@ -78,10 +86,10 @@ def separate_events(dataframe): #takes read in dataframe and separates multitrac
         data = data.T
         clusters = cluster.DBSCAN(eps=2).fit_predict(data) #algorithm outputs a list with an index corresponding to its cluster number
         df_clusters = pd.DataFrame(data=clusters,columns=['clust_val']) #make separate dataframe to access variables easily
-        clust_indices = [df_clusters.loc[df_clusters['clust_val'] == np.unique(clusters)[i]].index for i in range(0,len(np.unique(clusters)))] #entries are lists of indices corresponding to a given cluster
+        clust_indices = [df_clusters.loc[df_clusters['clust_val'] == np.unique(clusters)[i]].index.to_numpy() for i in range(0,len(np.unique(clusters)))] #entries are lists of indices corresponding to a given cluster
         for j in range(0,len(clust_indices)): #loop adds clusters as individual events to dataframe
             new_hit = pd.DataFrame({"event_number": events[i]+j, "timestamp": dataframe['timestamp'][events[i]], "npoints": len(clust_indices[j]), "column": [dataframe['column'][events[i]][clust_indices[j]]], "row": [dataframe['row'][events[i]][clust_indices[j]]], "BCID": [dataframe['BCID'][events[i]][clust_indices[j]]], "tot": [dataframe['tot'][events[i]][clust_indices[j]]], "sum_tot": np.sum(dataframe['tot'][events[i]][clust_indices[j]]), "num_clusters": len(clust_indices)},  index=[events[i]+j]) #adds new row for separated event from cluster
-            dataframe = dataframe.append(new_hit, ignore_index=True)
+            dataframe = dataframe.append(new_hit, ignore_index=True,  sort=True)
 
     for i in range(0,len(events)): #drops the original multi-track events that were separated into clusters
         dataframe = dataframe.drop(dataframe.loc[dataframe.index==events[i]].index[0]) #removes multiple track events bypassing reassigned indices
@@ -105,3 +113,64 @@ def extract_parameters_from_h5_file(input_file):
     df['npoints'] = [len(df['column'][j]) for j in range(0,n)]
     df['sum_tot'] = [np.sum(df['tot'][j]) for j in range(0,n)]
     return df
+'''
+def quick_h5_extract(input_file):
+    df_hits = pd.read_hdf(input_file, "Hits") #generates pandas dataframe of Hit-level data
+    df_meta = pd.read_hdf(input_file, "meta_data") #generates dataframe of metadata
+    df_meta['timestamp'] = df_meta['timestamp_start']
+    df_meta = df_meta.drop(columns = ['error_code','timestamp_stop','timestamp_start'])
+    df = df_meta.loc[df_meta['event_number'].isin(df_hits['event_number'])==True] #find events numbers that match
+    df = df.loc[df.duplicated(['event_number']) == False] #remove duplicate numbers
+    df.index = [i for i in range(0,len(df))]
+    df_hits = df_hits.loc[df_hits['event_number'].isin(df_meta['event_number'])==True]
+    df_hits.index = [i for i in range(0,len(df_hits))]
+    events = df['event_number'].to_numpy()
+    n = len(events)
+    df['column'], df['row'], df['tot'], df['BCID'] = np.array(df_hits.groupby(['event_number'])['column'].apply(list)),np.array(df_hits.groupby(['event_number'])['row'].apply(list)),np.array(df_hits.groupby(['event_number'])['tot'].apply(list)),np.array(df_hits.groupby(['event_number'])['relative_BCID'].apply(list))
+    df['column']=df['column'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['row']=df['row'].apply(lambda x: np.array(x,dtype=np.uint16))
+    df['tot']=df['tot'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['BCID']=df['BCID'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['npoints'] = [len(df['column'][j]) for j in range(0,n)]
+    df = df.loc[df['npoints']<10000]
+    df.index = [i for i in range(0,len(df))]
+    df['sum_tot'] = [np.sum(df['tot'][j]) for j in range(0,len(df))]
+    return df
+'''
+###Following scales fastest for large datasets
+def quick_h5_extract(input_file):
+    df_hits = pd.read_hdf(input_file, "Hits") #generates pandas dataframe of Hit-level data
+    df_meta = pd.read_hdf(input_file, "meta_data") #generates dataframe of metadata
+    df_meta['timestamp'] = df_meta['timestamp_start']
+    df_meta = df_meta.drop(columns = ['error_code','timestamp_stop','timestamp_start'])
+    df = df_meta.loc[df_meta['event_number'].isin(df_hits['event_number'])==True] #find events numbers that match
+    df = df.loc[df.duplicated(['event_number']) == False] #remove duplicate numbers
+    df.index = [i for i in range(0,len(df))]
+    df_hits = df_hits.loc[df_hits['event_number'].isin(df_meta['event_number'])==True]
+    df_hits.index = [i for i in range(0,len(df_hits))]
+    events = df['event_number'].to_numpy()
+    n = len(events)
+    df_column = pd.DataFrame({'event_number': df_hits['event_number'], 'column': df_hits['column']})
+    df_row = pd.DataFrame({'event_number': df_hits['event_number'], 'row': df_hits['row']})
+    df_tot = pd.DataFrame({'event_number': df_hits['event_number'], 'tot': df_hits['tot']})
+    df_BCID = pd.DataFrame({'event_number': df_hits['event_number'], 'BCID': df_hits['relative_BCID']})
+    df['column'] = make_hits_arrays(df_column,'column')['column']
+    df['column']=df['column'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['row'] = make_hits_arrays(df_row,'row')['row']
+    df['row']=df['row'].apply(lambda x: np.array(x,dtype=np.uint16))
+    df['tot'] = make_hits_arrays(df_tot,'tot')['tot']
+    df['tot']=df['tot'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['BCID'] = make_hits_arrays(df_BCID,'BCID')['BCID']
+    df['BCID']=df['BCID'].apply(lambda x: np.array(x,dtype=np.uint8))
+    df['npoints'] = [len(df['column'][j]) for j in range(0,n)]
+    df = df.loc[df['npoints']<10000]
+    df.index = [i for i in range(0,len(df))]
+    df['sum_tot'] = [np.sum(df['tot'][j]) for j in range(0,len(df))]
+    return df
+
+def make_hits_arrays(df,col):
+         keys, values = df.sort_values('event_number').values.T
+         ukeys, index = np.unique(keys, True)
+         arrays = np.split(values, index[1:])
+         df2 = pd.DataFrame({'event_number':ukeys, '%s'%(col):[np.array(a) for a in arrays]})
+         return df2
